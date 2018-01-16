bf514bfd62e2c6a8933a5e20a0f1ee24e442646f
猫猫很毛 <sonysuo1993@gmail.com>
2018-01-16 14:59:07 -0800

    update outlier

--- Exercise2.py
+++ Exercise2.py
@@ -11,12 +11,10 @@ url = 'https://en.wikipedia.org/w/index.php?title=Democracy_Index&oldid=82077680
 html = requests.get(url).content
 df_list = pd.read_html(html)
 df = df_list[-3]
-print(df)
 df.columns = df.iloc[0]
 df = df[1:]
 df.to_csv('my data.csv')
 #%%
-df.dtypes
 df['Category'].mode()
 #%%
 toNum=lambda x:pd.to_numeric(x)
@@ -26,8 +24,19 @@ democracy.dtypes
 pd.plotting.scatter_matrix(democracy,figsize=(12, 12))
 plt.show()
 #%%
-plot,dataBP=democracy['Electoral process and pluralism'].plot.box(vert=False,return_type='both')
-[value.get_xdata() for value in dataBP["boxes"]]
+df[['Score', 'Electoral process and pluralism', 'Functioning of government', 'Political participation', 'Political culture', 'Civil liberties']] = df[['Score', 'Electoral process and pluralism', 'Functioning of government', 'Political participation', 'Political culture', 'Civil liberties']].apply(pd.to_numeric)
 #%%
-[value.get_xdata() for value in dataBP["caps"]] 
-df['Electoral process and pluralism'].max()
+plot,dataBP=democracy['Electoral process and pluralism'].plot.box(vert=False,return_type='both')
+len([flier.get_xdata() for flier in dataBP["fliers"]][0])
+#%%
+plot,dataBP2=democracy['Functioning of government'].plot.box(vert=False,return_type='both')
+len([flier.get_xdata() for flier in dataBP2["fliers"]][0])
+#%%
+plot,dataBP3=democracy['Political participation'].plot.box(vert=False,return_type='both')
+len([flier.get_xdata() for flier in dataBP3["fliers"]][0])
+#%%
+plot,dataBP4=democracy['Political culture'].plot.box(vert=False,return_type='both')
+len([flier.get_xdata() for flier in dataBP4["fliers"]][0])
+#%%
+plot,dataBP5=democracy['Civil liberties'].plot.box(vert=False,return_type='both')
+len([flier.get_xdata() for flier in dataBP5["fliers"]][0])
